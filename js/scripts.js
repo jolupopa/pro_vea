e4499a5a6a25912f80721953253e14b498eae6c6
jolupopa <jlportilla76@gmail.com>
2020-04-05 13:15:39 -0500

    selectores de dia y hora

--- js/scripts.js
+++ js/scripts.js
@@ -197,7 +197,46 @@ $(function(){
 		$('.contenedor-buscar').removeClass('d-none');
 	});
 
-	
+	/*----------------------------------------------------*/
+	/*  sidebar comparar
+	/*----------------------------------------------------*/
+	$('.datepicker').pickadate({
+        monthsFull: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
+        monthsShort: ['ene', 'feb', 'mar', 'abr', 'may', 'jun', 'jul', 'ago', 'sep', 'oct', 'nov', 'dic'],
+        weekdaysFull: ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'],
+        weekdaysShort: ['Dom', 'Lun', 'Mar', 'Mié', 'Jue', 'Vie', 'Sáb'],
+        today: 'Hoy',
+        clear: 'Limpiar',
+        close: 'Cerrar',
+        labelMonthNext: 'Siguiente mes',
+        labelMonthPrev: 'Mes anterior',
+        labelMonthSelect: 'Seleccione un mes',
+        labelYearSelect: 'Seleccione un año',
+        firstDay: 1,
+        format: 'dddd, dd !de mmmm !de yyyy',
+        formatSubmit: 'dd/mm/yyyy',
+        selectYears: true,
+        selectMonths: true,
+        min: true,
+        max: 30,
+        onStart: function () {
+            var date = new Date();
+            this.set('select', [date.getFullYear(), date.getMonth(), date.getDate()]);
+        }
+    });
+
+    /*-----------------------------------------------------------
+      6. INICIANDO "picker.time.js"
+      -------------------------------------------------------------*/
+    $('.timepicker').pickatime({
+        clear: 'Borrar',
+        format: 'hh:i A',
+        interval: 120,
+        min: [8, 00],
+        max: [16, 00]
+
+    });
+
 
 
 })
