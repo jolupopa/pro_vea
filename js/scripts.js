2079991756efb06ce345b0d86d573799ccd1ba54
jolupopa <jlportilla76@gmail.com>
2020-04-05 19:00:25 -0500

    detail full

--- js/scripts.js
+++ js/scripts.js
@@ -235,7 +235,21 @@ $(function(){
         min: [8, 00],
         max: [16, 00]
 
-    });
+	});
+	
+	/*-----------------
+    INICIANDO VENOBOX
+    -----------------*/
+	$('.venobox-video').venobox({
+		autoplay: true,
+		bgcolor: 'rgba(255, 255, 255, 0.4)',
+		border: '5px',
+		closeColor: '#fff',
+		overlayColor: 'rgba(12, 60, 22, 0.83)',
+		spinner: 'three-bounce'
+	});
+
+
 
 
 
